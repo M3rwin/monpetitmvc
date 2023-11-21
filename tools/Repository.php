d244e738c56845283fba9b80cea1269b921899f9
marco.clin <marco.clin@CP-SIO-W11-PG10.bts.sio>
2023-11-21 11:58:53 +0100

    affichage asynchrone clients avec AJAX

--- tools/Repository.php
+++ tools/Repository.php
@@ -146,4 +146,35 @@ class Repository {
         return $this->traiteFindBy($methode, array_values($params));
     }
     
+    public function modifieTable($objet){
+        $tobjet = $this->object2Array($objet);
+        $parametres = array();
+        $sql = "update " . $this->table . " set ";
+        foreach($tobjet as $cle => $valeur){
+            if($cle != "id"){
+                if ($this->gereNull($valeur)){
+                    $sql .= $cle . "= null ,";
+                }else{
+                    $sql .= $cle . "= :" . $cle . " ,";
+                    $parametres[$cle]=$valeur;
+                }
+            }
+        }
+        $sql = substr($sql, 0, -1) . " where id =" . $tobjet['id'];
+        $unObjetPDO = Connexion::getConnexion();
+        $req = $unObjetPDO->prepare($sql);
+        $req->execute($parametres);
+    }
+    
+    public function object2Array($objet){
+        $tObjet = (array)$objet;
+        $tabloRetour = array();
+        foreach($tObjet as $cle => $valeur){
+            $cle = str_replace("\0", "", $cle);
+            $cle = str_replace($this->classeNameLong, "", $cle);
+            $tabloRetour[$cle] = $valeur;
+        }
+        return $tabloRetour;
+    }
+    
 }
